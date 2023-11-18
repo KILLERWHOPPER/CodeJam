6a8f4f8561e90d731ee26acf567e5d8565108cd3
Alison Quach <alisonquato@gmail.com>
2023-11-18 11:49:48 -0500

    Text to Speech

--- Assets/OpenAI.cs
+++ Assets/OpenAI.cs
@@ -1,26 +1,71 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class OpenAI : MonoBehaviour
 {
-    class RequestData
+    class RequestMicrophoneData
     {
-        public string animation;
         public string dialogue;
+    }
+
+    public class ErrorResponse
+    {
+        public string error;
     }
 
     string Base = "http://10.75.0.150:5000";
     string animation = "";
     string dialogue = "";
 
-    async Task SoundAsync()
+    IEnumerator SoundAsync()
     {
-        RequestData data = new RequestData();
-        data.animation = animation;
+        string url = Base + "/";
+        RequestMicrophoneData data = new RequestMicrophoneData();
         data.dialogue = dialogue;
+
+        string jsonData = JsonUtility.ToJson(data);
+        byte[] byteData = System.Text.Encoding.UTF8.GetBytes(jsonData);
+
+        Debug.Log("sending request...");
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        request.uploadHandler = new UploadHandlerRaw(byteData);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        using (request)
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                if (request.responseCode == 400)
+                {
+                    string response = request.downloadHandler.text;
+                    ErrorResponse errorResponse = JsonUtility.FromJson<ErrorResponse>(response);
+                    Debug.LogError("Server Error: " + errorResponse.error);
+                }
+                else
+                {
+                    Debug.Log("Error: " + request.error);
+                }
+            }
+            else
+            {
+                Debug.Log("response received for " + dialogue + ", loading image");
+                string response = request.downloadHandler.text;
+                byte[] imageData = Convert.FromBase64String(response);
+
+                //do stuff with data
+            }
+        }
     }
+
+    
 
 
     // Start is called before the first frame update
---
+++ Assets/TextToSpeech.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using Amazon.Runtime;
+using System.IO;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+
+public class TextToSpeech : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    // Start is called before the first frame update
+    private async void Start()
+    {
+        var credentials = new BasicAWSCredentials("", "");
+        var client = new AmazonPollyClient(credentials, RegionEndpoint.USEast1);
+
+        var request = new SynthesizeSpeechRequest()
+        {
+            Text = "Welcome to Code Jam 13!",
+            Engine = Engine.Neural,
+            VoiceId = VoiceId.Arthur,
+            OutputFormat = OutputFormat.Mp3
+        };
+
+        try
+        {
+            var response = await client.SynthesizeSpeechAsync(request);
+            WriteIntoFile(response.AudioStream);
+        }
+        catch (Amazon.Runtime.Internal.HttpErrorResponseException ex)
+        {
+            Debug.Log(ex.Message);
+        }
+
+
+        using (var www = UnityWebRequestMultimedia.GetAudioClip($"{Application.persistentDataPath}/audio.mp3", AudioType.MPEG))
+        {
+            var op = www.SendWebRequest();
+            while(!op.isDone)
+            {
+                await Task.Yield();
+            }
+
+            var clip = DownloadHandlerAudioClip.GetContent(www);
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+
+    private void WriteIntoFile(Stream stream)
+    {
+        using (var fileStream = new FileStream($"{Application.persistentDataPath}/audio.mp3", FileMode.Create))
+        {
+            byte[] buffer = new byte[4 * 1024];
+            int bytesRead;
+
+            while((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fileStream.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
